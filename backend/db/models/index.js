c040290af5500340869962b563c3936c826ff3cf
Corbin <corbindbullard@gmail.com>
2024-03-08 16:44:53 -0500

    force true

--- backend/db/models/index.js
+++ backend/db/models/index.js
@@ -16,7 +16,7 @@ if (config.use_env_variable) {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
-sequelize.sync({ force: false }).then(() => {
+sequelize.sync({ force: true }).then(() => {
   console.log("Database & tables created!");
 });
 
